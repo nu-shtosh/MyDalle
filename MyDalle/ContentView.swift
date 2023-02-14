97e68a62cdd8adfec68f5fe857d8716501824b3d
ILIA <dubenskiiilia@gmail.com>
2023-02-14 16:03:11 +0100

    Initial Commit

---
+++ MyDalle/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MyDalle
+//
+//  Created by Илья Дубенский on 14.02.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyDalle/MyDalleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyDalleApp.swift
+//  MyDalle
+//
+//  Created by Илья Дубенский on 14.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct MyDalleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
